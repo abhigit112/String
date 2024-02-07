404a89ccf8f36b16fc14b7d537a02376e0762ab7
Abhishek Raghuvanshi <122772762+abhigit112@users.noreply.github.com>
2024-02-07 18:25:18 +0530

    Update Capitalizeeachword.cpp

--- Capitalizeeachword.cpp
+++ Capitalizeeachword.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a;
+    getline(cin,a);
+    for(int i=0;i<a.length();i++)
+    {
+        if(i==0 || a[i-1]==' ')
+        {
+            a[i]=toupper(a[i]);
+        }
+    }
+    cout<<a;
+}
